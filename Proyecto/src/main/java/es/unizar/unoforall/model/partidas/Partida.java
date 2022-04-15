68c4fc46966a7599603a2a69be36e51fb9e777d4
Marcos Ilarraza <797720@unizar.es>
2022-04-15 12:26:15 +0200

    Efecto Rayos X
    Se ha implementado el efecto de jugar la carta Rayos X junto a dos variables.
    Se ha implementado la función <getRayosX> para recibir la carta vista. Se le pasa el objeto jugador del usuario y solo devuelve la carta si es el usuario que jugó la carta en el turno anterior(). Tiene guardas para sentido horario y antihorario.

--- Proyecto/src/main/java/es/unizar/unoforall/model/partidas/Partida.java
+++ Proyecto/src/main/java/es/unizar/unoforall/model/partidas/Partida.java
@@ -29,6 +29,10 @@ public class Partida {
 	private boolean terminada;	
 	private Date fechaInicio; //Fecha de inicio de la partida (Ya en formato sql porque no la necesita el frontend en este punto). 
 	
+	//Variables para extraer resultados de efectos
+	private Carta vistaPorRayosX;
+	private boolean efectoRayosX;
+	
 	private static final int MAX_ROBO_ATTACK = 10;
 	
 	public Partida(String error) {			//Para construir una partida con error = true
@@ -224,6 +228,10 @@ public class Partida {
 						
 					case rayosX:
 						//TODO
+						List<Carta> mano = siguienteJugador().getMano();
+						Collections.shuffle(mano);
+						vistaPorRayosX = mano.get(0);
+						efectoRayosX = true;
 						break;
 						
 					case reversa:
@@ -384,6 +392,18 @@ public class Partida {
 
 	public ConfigSala getConfiguracion() {
 		return configuracion;
+	}
+	
+	/**
+	 * @return			null si no se ha jugado una carta de rayosX el turno anterior o no eres quien la jugó.
+	 * 					la carta vista si se ha jugado por el jugador.
+	 */
+	public Carta getRayosX(Jugador jugador) {
+		if(efectoRayosX && (this.turno==0 && jugadores.indexOf(jugador)==configuracion.getMaxParticipantes()-1 
+				|| jugadores.indexOf(jugador)==this.turno-1)) {
+			return vistaPorRayosX;
+		}
+		return null;
 	}
 	
 }
